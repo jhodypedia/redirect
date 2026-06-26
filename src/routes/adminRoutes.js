6c023d1de802e8a5e72ab9372dc939e797a782d3
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2026-06-27 01:50:27 +0700

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,9 +4,9 @@ const adminController = require('../controllers/adminController');
 const asyncHandler = require('../utils/asyncHandler');
 const adminAuth = require('../middlewares/adminAuth');
 
-router.use(adminAuth);
-router.get('/links', asyncHandler(adminController.getLinks));
-router.get('/links/:id', asyncHandler(adminController.getLink));
-router.get('/links/:id/stats', asyncHandler(adminController.getStats));
+router.use(adminAuth); // Pastikan middleware auth berjalan
+
+// Route untuk merender halaman HTML
+router.get('/', asyncHandler(adminController.renderDashboard));
 
 module.exports = router;
